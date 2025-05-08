fa93cce602a69a98557b3fb0c388ed1af50ec554
Hemanth-kumar23 <hemantkumarr365@gmail.com>
2025-05-08 15:50:31 +0530

    today i did nth

--- Arraysjv.java
+++ Arraysjv.java
@@ -153,32 +153,32 @@
 //     }
 // }
 // // 9) subarray : ...
-public class Arraysjv {
+// public class Arraysjv {
 
-    public static void subarrays(int numbers[]) {
-        int ts = 0;
-        int sum = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            int start = i;
-            for (int j = i + 1; j < numbers.length; j++) {
-                int end = j;
-                int currentsum = 0;
-                for (int k = start; k <= end; k++) {
-                    System.out.print(numbers[k] + " ");
-                    currentsum += numbers[k];
-                }
-                sum += currentsum;
-                ts++;
-                System.out.println();
-            }
-            System.out.println();
-        }
-        System.out.println("total subarrays are : " + ts);
-        System.out.println("sum of sub array : " + sum);
-    }
+//     public static void subarrays(int numbers[]) {
+//         int ts = 0;
+//         int sum = 0;
+//         for (int i = 0; i < numbers.length; i++) {
+//             int start = i;
+//             for (int j = i + 1; j < numbers.length; j++) {
+//                 int end = j;
+//                 int currentsum = 0;
+//                 for (int k = start; k <= end; k++) {
+//                     System.out.print(numbers[k] + " ");
+//                     currentsum += numbers[k];
+//                 }
+//                 sum += currentsum;
+//                 ts++;
+//                 System.out.println();
+//             }
+//             System.out.println();
+//         }
+//         System.out.println("total subarrays are : " + ts);
+//         System.out.println("sum of sub array : " + sum);
+//     }
 
-    public static void main(String[] args) {
-        int numbers[] = {1, 2, 3, 4, 5, 6};
-        subarrays(numbers);
-    }
-}
+//     public static void main(String[] args) {
+//         int numbers[] = {1, 2, 3, 4, 5, 6};
+//         subarrays(numbers);
+//     }
+// }
